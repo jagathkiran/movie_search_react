e4bd5a738a049e66e9de2421aa79ddf4095715e7
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2026-01-04 18:18:53 +0530

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+
+	return (
+		<header>
+			<h1>Movie Search</h1>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+				</ul>
+			</nav>
+			<hr />
+		</header>
+	);
+}
+
+export default Header
